6e7cfde80230698d1239881e5a442374ae0f7747
Fenix4088 <fenix4088@gmail.com>
2021-12-25 20:42:59 +0200

    user registration

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,17 +1,38 @@
 import { TRequest } from '../types/common';
 import { Response } from 'express';
-import Role from '../models/role.model';
+import bcryptjs from 'bcryptjs';
 import User from '../models/user.model';
+import Role from '../models/role.model';
+
+import { IUser } from 'src/types/auth';
 interface IAuthController {
   users(req: TRequest<{}, {}>, res: Response): Promise<Response<any, Record<string, any>>>;
+  registaration(req: TRequest<IUser, {}>, res: Response): Promise<Response<any, Record<string, any>>>;
 }
 
 class AuthController implements IAuthController {
-  public registaration = async (req: TRequest<{}, {}>, res: Response) => {
+  public registaration = async (req: TRequest<IUser, {}>, res: Response) => {
     try {
+      const { username, password } = req.body;
+
+      const candidate = await User.findOne({ username });
+
+      if (candidate) {
+        return res.status(401).json({ message: `User: "${username}" already excisted` });
+      }
+
+      const hashedPassword = bcryptjs.hashSync(password, bcryptjs.genSaltSync(10));
+
+      const userRole = await Role.findOne({ role: 'USER' });
+
+      const user = new User({ username, password: hashedPassword, roles: [userRole?.value] });
+
+      await user.save();
+
+      return res.status(200).json({ message: `User "${username}" was created`, user });
     } catch (error) {
       console.log(error);
-      return res.sendStatus(500).json(error);
+      return res.status(400).json({ message: 'Registration error' });
     }
   };
 
@@ -19,85 +40,18 @@ class AuthController implements IAuthController {
     try {
     } catch (error) {
       console.log(error);
-      return res.sendStatus(500).json(error);
+      return res.status(400).json({ message: 'Login error' });
     }
   };
 
   public users = async (req: TRequest<{}, {}>, res: Response) => {
-    try {       
+    try {
       return res.status(200).json('users');
     } catch (error) {
       console.log(error);
       return res.status(500).json(error);
     }
   };
-
-  // public create = async (req: TRequest<Omit<ICreatePostBody, '_id'>, {}, true>, res: Response) => {
-  //   try {
-
-  //     const { author, title, content } = req.body;
-
-  //     const newPost = await PostServices.create({ author, title, content, picture: req.files?.picture });
-
-  //     res.status(200).send(newPost);
-  //   } catch (err: any) {
-  //     res.status(500).send(err.message);
-  //   }
-  // };
-
-  // public getPosts = async (req: TRequest<{}, IGetPosts>, res: Response) => {
-  //   const { id } = req.query;
-
-  //   try {
-  //     const getPostsResult = await PostServices.getPosts(id);
-
-  //     if (!getPostsResult) {
-  //       res.status(404).json(`Post by id: ${id} wasnt found`);
-  //       return;
-  //     }
-  //     res.status(200).json(getPostsResult);
-  //   } catch (e) {
-  //     res.status(500).send(e);
-  //   }
-  // };
-
-  // public update = async (req: TRequest<ICreatePostBody, {}>, res: Response) => {
-  //   const { author, title, content, picture, _id } = req.body;
-
-  //   if (!_id) {
-  //     res.status(400).json(`No post id`);
-  //   }
-
-  //   const updatedPost = await PostServices.update({ author, title, content, picture, _id });
-
-  //   if (!updatedPost) {
-  //     res.status(404).json(`Post with id: ${_id} not found`);
-  //   }
-
-  //   res.status(200).json(updatedPost);
-  // };
-
-  // public delete = async (req: TRequest<{}, IGetPosts>, res: Response) => {
-  //   try {
-  //     const { id } = req.query;
-
-  //     if (!id) {
-  //       res.status(404).send('Add id to delete a post');
-  //       return;
-  //     }
-
-  //     const deletedPost = await PostServices.delete(id);
-
-  //     if (!deletedPost) {
-  //       res.status(404).json(`Post by id: ${id} not found`);
-  //       return;
-  //     }
-
-  //     res.status(200).json(deletedPost);
-  //   } catch (err) {
-  //     res.status(500).send(err);
-  //   }
-  // };
 }
 
 export default new AuthController();
