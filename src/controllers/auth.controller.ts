2c376645953f8b3c465d9b89befd613cb934642c
Fenix4088 <fenix4088@gmail.com>
2021-12-26 17:20:38 +0200

    generate jwt when login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,6 +5,11 @@ import User from '../models/user.model';
 import Role from '../models/role.model';
 import { IUser } from 'src/types/auth';
 import { validationResult } from 'express-validator';
+import { Types } from 'mongoose';
+import jwt, { Secret } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 interface IAuthController {
   users(req: TRequest<{}, {}>, res: Response): Promise<Response<any, Record<string, any>>>;
@@ -43,8 +48,21 @@ class AuthController implements IAuthController {
     }
   };
 
-  public login = async (req: TRequest<{}, {}>, res: Response) => {
+  public login = async (req: TRequest<Omit<IUser, 'roles'>, {}>, res: Response) => {
     try {
+      const { username, password } = req.body;
+
+      const user = await User.findOne({ username });
+
+      if (!user) return res.status(400).json({ message: `Didnt find user ${username}` });
+
+      const isValidPassword = await bcryptjs.compare(password, user.password);
+
+      if (!isValidPassword) return res.status(400).json({ message: 'Incorect password' });
+
+      const token = this.generateToken(user._id, user.roles);
+
+      return res.status(200).json({ message: 'Login succesfful', token });
     } catch (error) {
       console.log(error);
       return res.status(400).json({ message: 'Login error' });
@@ -53,11 +71,16 @@ class AuthController implements IAuthController {
 
   public users = async (req: TRequest<{}, {}>, res: Response) => {
     try {
-      return res.status(200).json('users');
+      const users = await User.find();
+      return res.status(200).json(users);
     } catch (error) {
       console.log(error);
       return res.status(500).json(error);
     }
+  };
+
+  private generateToken = (id: Types.ObjectId, roles: string[]): string => {
+    return jwt.sign({ id, roles }, process.env.TOKEN_SECRET_KEY as Secret, { expiresIn: '24h' });
   };
 }
 
